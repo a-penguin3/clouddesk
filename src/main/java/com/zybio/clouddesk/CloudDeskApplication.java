6f45971d0d795852c274d7727ffbaed23bd3a690
zhangzihao <zhangzihao@zy-ivd.com>
2022-11-22 11:21:08 +0800

    初始化

---
+++ src/main/java/com/zybio/clouddesk/CloudDeskApplication.java
@@ -0,0 +1,13 @@
+package com.zybio.clouddesk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudDeskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloudDeskApplication.class, args);
+    }
+
+}
